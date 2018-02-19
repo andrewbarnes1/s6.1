c14ba30260f8ccf0eaac7698750262000fd7f40c
Student <student@C17775456-lnx.offcampusnetwork.co.uk>
2018-02-19 16:25:34 +0000

    Added in threads for the other LEDS. Unable to control behaviour.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ Thread worker;
 EventQueue queue ;
 
 DigitalOut red(LED_RED,1); /* initial state 1 led is off */
+DigitalOut green(LED_GREEN,0);
+DigitalOut blue(LED_BLUE,0);
+
 void flash_red(void) {
     while(1){
         red.write(1);
@@ -15,10 +18,30 @@ void flash_red(void) {
         wait(0.5);
     }
 }
+
+void flash_green(void) {
+  while(1){
+        green.write(0);
+        wait(0.5);
+        green.write(1);
+        wait(0.5);
+  }
+}
+
+void flash_blue(void){
+  while(1){
+    blue.write(0);
+    wait(0.5);
+    blue.write(1);
+    wait(0.5);
+  }
+}
 int main() {
 
-    // put your setup code here, to run once:
+    // Hard to determine the flashing of the threads. 
     worker.start( flash_red );
+    worker.start( flash_green );
+    worker.start( flash_blue );
 
     while(1) {
         // put your main code here, to run repeatedly:
